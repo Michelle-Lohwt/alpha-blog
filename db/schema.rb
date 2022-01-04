fc7cc1f8867620589b02586037774457955afef6
michelle <michellelohwt0799@gmail.com>
2022-01-04 19:19:11 +0800

    feat: migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_03_063359) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+  end
 
 end
