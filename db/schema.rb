36f3009e60631f93aa00d1799de62a39c70180c6
michelle <michellelohwt0799@gmail.com>
2022-01-04 19:23:49 +0800

    feat: timestamps created

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_063359) do
+ActiveRecord::Schema.define(version: 2022_01_04_111953) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
