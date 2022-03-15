3834ffc55843ff39a5809edef469201a6fb09389
michelle <michellelohwt0799@gmail.com>
2022-03-15 16:12:21 +0800

    feat: add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
+  # The :articles refers to the articles controller
   resources :articles, only: [:show, :index, :new, :create]
 end
